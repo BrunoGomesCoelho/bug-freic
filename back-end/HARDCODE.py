fe771695094fe2976c864c2b5d45a6273525a232
BrunoGomesCoelho <bruno.gomes.coelho@usp.br>
2019-08-24 19:17:33 -0300

    "Add back end folder"

---
+++ back-end/HARDCODE.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+
+SCHEDULE = """Fase 1:     Jan 2019 - Jan 2019     Revisão bibliográfica referente a avaliação de sistemas de aprendizado de máquina;
+Fase 2:     Feb 2019 - Mar 2019     Estudo sobre a ferramenta Weka e seus sistemas de aprendizado de máquina para classificação;
+Fase 3:    Mar 2019 - Jun 2019     Estudo do código Java dos classificadores na ferramenta Weka e identificação dos principais parâmetros;
+Fase 4:     Jun 2019 - Set 2019     Implementação de uma classe de otimização de parâmetros utilizando uma busca em grid;
+Fase 5:     Out 2019 - Nov 2019     Realização de experimentos com dados;
+Fase 6:     Dez 2019 - Dez 2019     Elaboração do relatório final e escrita de artigos.Revisão bibliográfica referente a avaliação de sistemas de aprendizado de máquina;"""
+
+FINANCES_DESCRIPTION = """Bolsa: 3 x 300,00
+Raspberry Pi 3 B: 259,00
+Servidor Digital Ocean: 3 x 20,00"""
+
+DISCIPLINES = """Inteligência artificial, Introdução a Machine Learning, Introdução a Ciência de Dados
+Algoritmos e Estruturas de Dados, Projeto e Análise de Algoritmos
+Introdução ao Desenvolvimento Web, Web Semântica, Redes de Computadores
+Internet das Coisas, Sistemas Distribuídos, Programação Paralela, Redes de Computadores"""
+
+SITUACAO_COLORS = np.array(["green", "yellow", "red"])
+SITUACAO_DESC = np.array(["Projeto aprovado", 
+    "Alunos selecionados, aguardando recursos financeiros",
+    "Projeto recusado"])
+
+
---
+++ back-end/app.py
@@ -0,0 +1,32 @@
+#!flask/bin/python
+import pandas as pd
+from flask import Flask, jsonify
+from HARDCODE import *
+
+DATA_FOLDER = "../data/"
+PROJECT_FILE = "projetos.csv" 
+
+app = Flask(__name__)
+
+@app.route('/projects/<int:quantity>', methods=["GET"])
+def all_projects(quantity):
+    df = pd.read_csv(DATA_FOLDER+PROJECT_FILE)
+    df.drop(columns=["2_Bolsa"], inplace=True)
+    situacao_idx = np.random.randint(3, size=len(df))
+    df["Situacao_Cor"] = SITUACAO_COLORS[situacao_idx]
+    df["Situacao_Desc"] = SITUACAO_DESC[situacao_idx]
+    df["Disciplinas"] = DISCIPLINES
+    df["Cronograma"] = SCHEDULE
+    df["Gastos_Desc"] = FINANCES_DESCRIPTION 
+    df["Gastos"] = (np.random.randint(3, 10, size=len(df))*100*12).astype(str)
+    df["Gastos"] += ",00"
+    result = {}
+    for index, row in df.iterrows():
+        result[index] = dict(row)
+    print(result)
+    return jsonify(result)
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+
