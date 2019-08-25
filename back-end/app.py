c7cc0eaffa9d8726d3d008cf2adb3c3ef8344e17
BrunoGomesCoelho <bruno.gomes.coelho@usp.br>
2019-08-24 21:16:28 -0300

    "Add students to app"

--- back-end/app.py
+++ back-end/app.py
@@ -1,12 +1,17 @@
 #!flask/bin/python
 from flask import Flask
 import projects
+import students
 
 app = Flask(__name__)
 
 @app.route('/projects/', methods=["GET"])
 def all_projects():
     return projects._all_projects()
+
+@app.route('/students/', methods=["GET"])
+def all_students():
+    return students._all_students()
 
 if __name__ == '__main__':
     app.run(debug=True)
---
+++ back-end/students.py
@@ -0,0 +1,30 @@
+#!flask/bin/python
+import pandas as pd
+from flask import Flask, jsonify
+from HARDCODE import *
+
+DATA_FOLDER = "../data/"
+PROJECT_FILE = "projetos.csv" 
+
+def _all_students():
+    temp = pd.read_csv(DATA_FOLDER+PROJECT_FILE)
+    size = len(temp)
+
+    cols = ["NUSP", "Nome", "Media", "Semestre", "Creditos_Acumulados"]
+    df = pd.DataFrame(columns=cols)
+
+    df["Nome"] = temp["Bolsista"]
+    df["NUSP"] = np.random.randint(8000000, 9999999, size=size)
+    df["Media"] = np.random.normal(7.0, 2.1, size=size)
+    df["Semestre"] = np.random.randint(1, 8, size=size)
+    df["Creditos_Acumulados"] = np.random.randint(12, 156, size=size)
+
+    for col in cols:
+        df[col] = df[col].astype(str)
+    df["Media"] = df["Media"].apply(lambda x: x[:4])
+
+    result = {}
+    for index, row in df.iterrows():
+        result[index] = dict(row)
+    return jsonify(result)
+
