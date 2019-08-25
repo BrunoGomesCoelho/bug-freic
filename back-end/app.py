aa6b44c46221b55147da107fb86f077365ffbbfc
BrunoGomesCoelho <bruno.gomes.coelho@usp.br>
2019-08-25 03:41:08 -0300

    "add search endpoint"

--- back-end/app.py
+++ back-end/app.py
@@ -8,32 +8,31 @@ app = Flask(__name__)
 df_projects = projects._all_projects()
 df_students = students._all_students()
 
-@app.route('/projects/', methods=["GET"])
-def all_projects():
+
+def process_dataframe(df):
     result = {}
-    for index, row in df_projects.iterrows():
+    for index, row in df.iterrows():
         result[index] = dict(row)
 
     response = jsonify(result)
     response.headers.add('Access-Control-Allow-Origin', '*')
     return response
+ 
+
+@app.route('/projects/', methods=["GET"])
+def all_projects():
+    return process_dataframe(df_projects)
 
 
 @app.route('/students/', methods=["GET"])
 def all_students():
-    result = {}
-    for index, row in df_students.iterrows():
-        result[index] = dict(row)
-
-    response = jsonify(result)
-    response.headers.add('Access-Control-Allow-Origin', '*')
-    return response
+    return process_dataframe(df_students)
 
 
-@app.route('/search/<string:hi>', methods=["GET"])
-def search(hi):
-    print(hi)
-    return None
+@app.route('/search/<string:title>', methods=["GET"])
+def search(title):
+    idx = df_projects["Titulo_do_Projeto"].str.contains(title)
+    return process_dataframe(df_projects[idx])
 
 
 if __name__ == '__main__':
