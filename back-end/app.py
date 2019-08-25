ff13a317fe408094282b8fe6c97c3129cc2aef2d
mvjq <mviniciusjunqueira@gmail.com>
2019-08-24 23:04:44 -0300

    Fix CORS

--- back-end/app.py
+++ back-end/app.py
@@ -7,11 +7,15 @@ app = Flask(__name__)
 
 @app.route('/projects/', methods=["GET"])
 def all_projects():
-    return projects._all_projects()
+    response = projects._all_projects()
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 @app.route('/students/', methods=["GET"])
 def all_students():
-    return students._all_students()
+    response = students._all_students()
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    return response
 
 if __name__ == '__main__':
     app.run(debug=True)
